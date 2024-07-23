fedf1a8580f0eb55014d5749b3c1d1d7baee0ed4
haripriya1705 <haripriya.kottu@gmail.com>
2024-07-23 13:05:12 +0100

    Enabled CORS for the local origin

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.use(cors())
+app.use(cors({
+  origin: 'http://127.0.0.1:5500'
+}))
 app.use(express.json())
 app.use('/fruits', fruitRouter)
 
